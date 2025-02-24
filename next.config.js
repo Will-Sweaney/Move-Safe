65a4444153854049e7339a8bdf26383f9972339d
Will Sweaney <wmsweaney@gmail.com>
2025-02-24 17:32:02 +0000

    check prod

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const production = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
 	output: 'export',
+  basePath: production ? '/Move-Safe' : '',
   images: {
     unoptimized: true
   }
