0d738ada76164f736d9f28c56c963316f94d57aa
Will Sweaney <wmsweaney@gmail.com>
2025-02-24 17:02:58 +0000

    fix

--- move-safe-webapp/next.config.js
+++ move-safe-webapp/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
+	basePath: '/Move-Safe',
 	output: 'export',
+	reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
