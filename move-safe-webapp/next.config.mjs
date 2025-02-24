659e057c460726064532d9193bd7e747022c5bfe
Will Sweaney <wmsweaney@gmail.com>
2025-02-24 14:49:31 +0000

    static site

--- move-safe-webapp/next.config.mjs
+++ move-safe-webapp/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+	output: 'export',
+	images: {
+		unoptimized: true,
+	},
+	basePath: '/move-safe',
+};
 
 export default nextConfig;
